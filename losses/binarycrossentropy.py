e8ee248f63594ab3f7f0a9d65dcf8a82cf8adc67
Abtinz <abtinzandi@gmail.com>
2023-06-01 13:12:43 -0700

    binary cross entropy loss and its formula and license of formula

--- losses/binarycrossentropy.py
+++ losses/binarycrossentropy.py
@@ -1,21 +1,21 @@
 import numpy as np
 
+#this is built based on ./losses/binaryCrossEntropy.png formula
 class BinaryCrossEntropy:
     def __init__(self) -> None:
         pass
 
+    #Computes the binary cross entropy loss.
+    #    arguments:
+    #        y --> real labels and  batch_size
+    #        y_hat --> predicted labels and batch size
+    #    returns:  binary cross entropy loss !
     def compute(self, y_hat: np.ndarray, y: np.ndarray) -> float:
-        """
-        Computes the binary cross entropy loss.
-            args:
-                y: true labels (n_classes, batch_size)
-                y_hat: predicted labels (n_classes, batch_size)
-            returns:
-                binary cross entropy loss
-        """
-        # TODO: Implement binary cross entropy loss
-        batch_size = None
-        cost = None
+        
+        epsilon = 1e-7  # small value to avoid division by zero, its not to small for process!
+
+        cost = ((-1/  y.shape[1]) * np.sum(y * np.log(y_hat + epsilon) + (1 - y) * np.log(1 - y_hat + epsilon)))
+        
         return np.squeeze(cost)
 
     def backward(self, y_hat: np.ndarray, y: np.ndarray) -> np.ndarray:
@@ -27,7 +27,7 @@ class BinaryCrossEntropy:
             returns:
                 derivative of the binary cross entropy loss
         """
-        # hint: use the np.divide function
-        # TODO: Implement backward pass for binary cross entropy loss
-        return None
+        epsilon =1e-14  
+        dZ = (y_hat - y) / (y_hat * (1 - y_hat) + epsilon)
+        return dZ
 
