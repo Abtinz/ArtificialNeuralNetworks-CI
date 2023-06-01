90d6a4b3b6f0a5dd770a3ba9952219af6b6f319a
Abtinz <abtinzandi@gmail.com>
2023-06-01 13:50:54 -0700

    MeanSquaredError is implemented

--- losses/meansquarederror.py
+++ losses/meansquarederror.py
@@ -13,9 +13,9 @@ class MeanSquaredError:
             returns:
                 mean squared error loss
         """
-        # TODO: Implement mean squared error loss
-        batch_size = None
-        cost = None
+        # Implement mean squared error loss
+        batch_size = y_pred.shape[1]
+        cost = (1 / (2 * batch_size)) * np.sum((y_pred - y_true) ** 2)
         return np.squeeze(cost)
     
     def backward(self, y_pred, y_true):
@@ -27,5 +27,7 @@ class MeanSquaredError:
             returns:
                 derivative of the mean squared error loss
         """
-        # TODO: Implement backward pass for mean squared error loss
-        return None
+        # Implement backward pass for mean squared error loss
+        batch_size = y_pred.shape[1]
+        d_loss = (1 / batch_size) * (y_pred - y_true)
+        return d_loss
