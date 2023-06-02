931d687036abf56f89a375e6622d9798bc27a22a
Abtinz <abtinzandi@gmail.com>
2023-06-02 10:48:19 +0330

    implementing gradient descent

--- optimizers/gradientdescent.py
+++ optimizers/gradientdescent.py
@@ -1,27 +1,26 @@
-# TODO: Implement the gradient descent optimizer
+#Gradient Descent optimizer.
+#this class will help us to implement gradient descent 
+#slf parameters -> learning rate and layers_list : dictionary of layers name and layer object
+
 class GD:
+
     def __init__(self, layers_list: dict, learning_rate: float):
-        """
-        Gradient Descent optimizer.
-            args:
-                layers_list: dictionary of layers name and layer object
-                learning_rate: learning rate
-        """
         self.learning_rate = learning_rate
         self.layers = layers_list
     
+    
+    #Update the parameters of the layer.
+    #formula -> ./optimizers/GradientDescent.png
+    #  arguments:
+    #      grads: list of gradients for the weights and bias
+    #      name: name of the layer we want to update with gradient descent
+    #  returns: params: list of updated parameters
+    
     def update(self, grads, name):
-        """
-        Update the parameters of the layer.
-            args:
-                grads: list of gradients for the weights and bias
-                name: name of the layer
-            returns:
-                params: list of updated parameters
-        """
-        layer = self.layers[None]
-        params = []
-        #TODO: Implement gradient descent update
-        for None in range(len(grads)):
-            params.append(None - None * None)
-        return params
+       
+        layer = self.layers[name]
+        updated_parameters = []
+        # Update the weights and bias using gradient descent
+        for index in range(len(grads)):
+            updated_parameters.append((layer.params[index] - self.learning_rate * grads[index]))
+        return updated_parameters
