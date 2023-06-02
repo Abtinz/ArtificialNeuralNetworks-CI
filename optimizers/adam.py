335da7e4a9e8f2a5e9b268128e00e5a2f7ea06c6
Abtinz <abtinzandi@gmail.com>
2023-06-02 11:01:54 +0330

    Adam implemented

--- optimizers/adam.py
+++ optimizers/adam.py
@@ -1,7 +1,6 @@
 import numpy as np
 from .gradientdescent import GradientDescent
 
-# TODO: Implement Adam optimizer
 class Adam:
     def __init__(self, layers_list, learning_rate=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
         self.layers = layers_list
@@ -11,21 +10,25 @@ class Adam:
         self.epsilon = epsilon
         self.V = {}
         self.S = {}
-        for None in layers_list:
-            # TODO: Initialize V and S for each layer (v and s are lists of zeros with the same shape as the parameters)
-            v = [None for p in layers_list[None].parameters]
-            s = [None for p in layers_list[None].parameters]
-            self.V[None] = v
-            self.S[None] = s
+        for name in layers_list:
+            # Initialize V and S for each layer (v and s are lists of zeros with the same shape as the parameters)
+            v = [np.zeros_like(parameter) for parameter in layers_list[name].parameters]
+            s = [np.zeros_like(parameter) for parameter in layers_list[name].parameters]
+            self.V[name] = v
+            self.S[name] = s
         
     def update(self, grads, name, epoch):
-        layer = self.layers[None]
-        params = []
-        # TODO: Implement Adam update
-        for None in range(len(grads)):
-            self.V[None][None] = None * None + (1 - None) * None
-            self.S[None][None] = None * None  +(1 - None) * np.square(None)
-            self.V[None][None] /= (1 - np.power(self.beta1, epoch)) # TODO: correct V
-            self.S[None][None] /= (1 - np.power(self.beta2, epoch)) # TODO: correct S
-            params.append(None - None * None / (np.sqrt(None) + None))
-        return params
+        layer = self.layers[name]
+        parameters = []
+        # Adam update -> param = param - learning_rate * V_corrected / (sqrt(S_corrected) + epsilon).
+        for i in range(len(grads)):
+            #V correction
+            self.V[name][i] = self.beta1 * self.V[name][i] + (1 - self.beta1) * grads[i]
+            V_corrected = self.V[name][i] / (1 - np.power(self.beta1, epoch))
+            #S correction
+            self.S[name][i] = self.beta2 * self.S[name][i] + (1 - self.beta2) * np.square(grads[i])           
+            S_corrected = self.S[name][i] / (1 - np.power(self.beta2, epoch))
+
+            parameter = layer.parameters[i] - self.learning_rate * V_corrected / (np.sqrt(S_corrected) + self.epsilon)
+            parameters.append(parameter)
+        return parameters
