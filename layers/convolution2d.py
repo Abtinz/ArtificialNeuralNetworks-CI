7db4211fdced4b17c8f8cb8c4fe3db549c582e6d
Abtinz <abtinzandi@gmail.com>
2023-06-08 23:23:13 +0330

    forward function is implemented

--- layers/convolution2d.py
+++ layers/convolution2d.py
@@ -1,4 +1,5 @@
 
+from math import sqrt
 import numpy as np
 
 # The Conv2D class is an implementation of a 2D convolutional layer for use in a convolutional neural network. 
@@ -79,34 +80,79 @@ class Conv2D:
         return (batch_size, chanel_count , output_height, output_width)
     
     def pad(self, A, padding, pad_value=0):
-        """
-        Pad the input with zeros.
-        args:
-            A: input to be padded
-            padding: tuple of padding for height and width
-            pad_value: value to pad with
-        returns:
-            A_padded: padded input
-        """
+        # Pad the input with zeros.
+        # args:
+        #     A: input to be padded
+        #     padding: tuple of padding for height and width
+        #     pad_value: value to pad with
+        # returns:
+        #     A_padded: padded input
+       
         A_padded = np.pad(A, ((0, 0), (padding[0], padding[0]), (padding[1], padding[1]), (0, 0)), mode="constant", constant_values=(pad_value, pad_value))
         return A_padded
     
-    #this function will convolve a slice of the input with the kernel.
-    #at first, we will do element-wise multiplication between the slice of input and the kernel!
-    #then we will do summation over all entries of the volume s.
-    #at last, we will Add bias to the result of the dot product.
+    # this function will convolve a slice of the input with the kernel.
+    # at first, we will do element-wise multiplication between the slice of input and the kernel!
+    # then we will do summation over all entries of the volume s.
+    # at last, we will Add bias to the result of the dot product.
     # arguments ->
     #       a_slic_prev: slice of the input data
     #       w_kernel: kernel
     #       bias: bias
-    #       returns:  convolved value
+    # returns:  convolved value
     def single_step_convolve(self, a_slic_prev, w_kernel, bias):
         
         summation_of_dot_product = np.sum( np.multiply(a_slic_prev, w_kernel) )
         convolved_product = float(summation_of_dot_product + bias)
         return convolved_product
 
+    # this function will help us to implement forwarding and pass for convolutional layer.
+    # arguments ->
+    #         A_prev: activations from previous layer (or input data)
+    #         A_prev.shape = (batch_size, H_prev, W_prev, C_prev)
+    #     returns:
+    #         A: output of the convolutional layer
+    
+    def forward(self, A_prev):
 
+        # Get kernel shape and bias
+        kernel_weights , bias = self.parameters
+        (batch_size , kernel_channels , kernel_height, kernel_width) = kernel_weights.shape
+        input_shape =  kernel_weights.shape
+        
+        # Get input shape
+        (batch_size,C_prev, H_prev, W_prev) = A_prev.shape
+    
+       
+        stride_h, stride_w = self.stride
+        padding_h, padding_w = self.padding
+
+        _, chanel_count , output_height, output_width = self.target_shape(input_shape)
+        
+        # Initialize output with zeros
+        output = np.zeros((batch_size, output_height , output_width , chanel_count))
+    
+        # Pad input tensor
+        A_prev_pad = self.pad(A_prev, (padding_h, padding_w))
+    
+        # processing over the batch
+        for i in range(batch_size):
+            # Loop over vertical axis of output volume
+            for h in range(output_height):
+                h_start = h * stride_h
+                h_end = h_start + kernel_height
+                # Loop over horizontal axis of output volume
+                for w in range(output_width):
+                    w_start = w * stride_w
+                    w_end = w_start + kernel_width
+                    # Loop over channels of output volume
+                    for c in range(kernel_channels):
+                        # Extract slice of input volume
+                        a_slice_prev = A_prev_pad[i, h_start:h_end, w_start:w_end, :]
+                        # Convolve slice of input volume with kernel and add bias
+                        output[i, h, w, c] = self.single_step_convolve(a_slice_prev, output_width[..., c], bias[..., c])
+    
+        return output
 
     def backward(self, dZ, A_prev):
         """
